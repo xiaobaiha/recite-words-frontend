0c31f5669b2674262f0fa35d59f5e8d3c47bfdd8
xiaobaiha <xiaobaihaha0001@163.com>
2018-06-28 22:47:08 +0800

    @dev: remove style overload

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-  config = rewireLess.withLoaderOptions({
-  })(config, env);
-  return config;
-};
