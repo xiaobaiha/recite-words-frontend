59d8b67fda4b230b39c40a2e4189146e4f8e1594
xiaobaiha <xiaobaihaha0001@163.com>
2018-08-15 21:34:45 +0800

    @dev update build

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import {Input} from 'antd';
-import {Layout} from 'antd';
-import HeaderCustom from './components/HeaderCustom/HeaderCustom';
-import FooterCustom from './components/FooterCustom/FooterCustom';
-import { CookiesProvider } from 'react-cookie';
-import './App.css';
-const {Content} = Layout;
+import React, { Component } from "react";
+import { Layout } from "antd";
+import HeaderCustom from "./components/HeaderCustom/HeaderCustom";
+import FooterCustom from "./components/FooterCustom/FooterCustom";
+import { CookiesProvider } from "react-cookie";
+import "./App.css";
+const { Content } = Layout;
 class App extends Component {
   render() {
-    const childrenWithProps = React.Children.map(this.props.children, (child) => React.cloneElement(child, {user: this.props.user}));
+    const childrenWithProps = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, { user: this.props.user })
+    );
     return (
       <CookiesProvider>
-      <Layout>
-        <HeaderCustom/>
-        <Layout style={{flexDirection: 'row'}}>
-          <Layout style={{padding: '0 24px 24px'}}>
-            <Content style={{margin: '0 16px', overflow: 'initial'}}>
-              {childrenWithProps}
-            </Content>
-          </Layout>
-
-        </Layout>
         <Layout>
-          <FooterCustom/>
+          <HeaderCustom />
+          <Layout style={{ flexDirection: "row" }}>
+            <Layout style={{ padding: "0 24px 24px" }}>
+              <Content style={{ margin: "0 16px", overflow: "initial" }}>
+                {childrenWithProps}
+              </Content>
+            </Layout>
+          </Layout>
+          <Layout>
+            <FooterCustom />
+          </Layout>
         </Layout>
-      </Layout>
       </CookiesProvider>
     );
   }
